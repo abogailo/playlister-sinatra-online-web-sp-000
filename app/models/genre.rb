a52824d667914597a354d9f6b9c59ba7b18d2d6d
Abagail Hudson <abagailanne@gmail.com>
2019-08-11 23:43:28 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,7 @@ class Genre < ActiveRecord::Base
   extend Slugifiable::ClassMethod
   include Slugifiable::InstanceMethod
 
-   has_many :song_genres
+  has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
 end
