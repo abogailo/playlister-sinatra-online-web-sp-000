3d3b4b811a702840913802cf502d7f23b11c3aab
Abagail Hudson <abagailanne@gmail.com>
2019-08-11 23:38:50 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,20 +1,11 @@
 class ArtistsController < ApplicationController
   get '/artists' do
     @artists = Artist.all
-
-     erb :"/artists/index"
+    erb :'/artists/index'
   end
-
-   get '/artists/new' do
-
-   end
-
-   post '/artists' do
-
-   end
 
    get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
-    erb :"/artists/show"
+    erb :'/artists/show'
   end
 end
