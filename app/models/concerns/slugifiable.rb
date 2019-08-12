7938df10c00556b5905f13be1fee929e8c4779fe
Abagail Hudson <abagailanne@gmail.com>
2019-08-12 00:09:27 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,16 +1,15 @@
 
 module Slugifiable
 
-   module ClassMethod
+  module ClassMethod
     def find_by_slug(slug)
       self.all.find{|obj| obj.slug == slug}
     end
   end
 
-   module InstanceMethod
+  module InstanceMethod
     def slug
       self.name.downcase.gsub(" ", "-")
     end
   end
-
- end
+end
