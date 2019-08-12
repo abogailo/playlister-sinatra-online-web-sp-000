2971f51bde9cc70cf6981599cb6cf57dfb63673c
Abagail Hudson <abagailanne@gmail.com>
2019-08-12 00:09:43 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,8 +3,7 @@ class Song < ActiveRecord::Base
   extend Slugifiable::ClassMethod
   include Slugifiable::InstanceMethod
 
-   belongs_to :artist
+  belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
-
  end
